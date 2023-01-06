0a734cfedf16eb470d0def9e2fb962b225008eb9
CurryOnPatat <99186666+CurryOnPatat@users.noreply.github.com>
2023-01-06 15:53:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,16 @@ if (fs.existsSync('./counter.txt')) {
   counter = parseInt(fs.readFileSync('./counter.txt', 'utf-8'));
 }
 
-app.get('/counter', (req, res) => {
-  counter++;
+app.get('/increment-counter', (req, res) => {
+   counter++;
   fs.writeFileSync('./counter.txt', String(counter));
   return res.status(200).json({ res: counter });
 });
 
-app.listen(3000, () => {
+app.get('/get-counter', (req, res) => {
+    return res.status(200).json({ res: counter });
+});
+
+app.listen(8080, () => {
   console.log('Listening on port 3000!');
 });
